52622e445eb3488b65808d51d599c8b8d913c7ce
saefulbarkah <saefulbarkah650@gmail.com>
2024-01-31 19:18:33 +0700

    feat(loader): add top loader

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NextTopLoader from "nextjs-toploader";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} overflow-y-scroll bg-light`}>
+        <NextTopLoader />
         <Layout>{children}</Layout>
       </body>
     </html>
