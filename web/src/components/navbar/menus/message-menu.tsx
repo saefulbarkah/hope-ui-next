367e39104eb6e875b3d18c46211ec04bc4c2d72d
saefulbarkah <saefulbarkah650@gmail.com>
2024-02-01 20:02:53 +0700

    fix: hydration UI

--- web/src/components/navbar/menus/message-menu.tsx
+++ web/src/components/navbar/menus/message-menu.tsx
@@ -10,13 +10,13 @@ import React from "react";
 export default function MessageMenu() {
   return (
     <Popover>
-      <PopoverTrigger>
+      <PopoverTrigger asChild>
         <Button variant={"ghost"} size={"icon"}>
           <Mail className="h-6 w-6 text-gray-600" />
         </Button>
       </PopoverTrigger>
       <PopoverContent sideOffset={30} className="w-52 text-center">
-        <p>on development....</p>
+        <p>in development....</p>
       </PopoverContent>
     </Popover>
   );
--- web/src/components/navbar/menus/notification-menu.tsx
+++ web/src/components/navbar/menus/notification-menu.tsx
@@ -10,13 +10,13 @@ import React from "react";
 export default function NotificationMenu() {
   return (
     <Popover>
-      <PopoverTrigger>
+      <PopoverTrigger asChild>
         <Button variant={"ghost"} size={"icon"}>
           <Bell className="h-6 w-6 text-gray-600" />
         </Button>
       </PopoverTrigger>
       <PopoverContent sideOffset={30} className="w-52 text-center">
-        <p>on development....</p>
+        <p>in development....</p>
       </PopoverContent>
     </Popover>
   );
